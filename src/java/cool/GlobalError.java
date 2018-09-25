9c0b6f6d42d22c800913d458d4e34e2fa08e3956
Supreet Singh <supreet51.cs@gmail.com>
2018-09-25 23:38:34 +0530

    stopped caring 11 commits ago

--- src/java/cool/GlobalError.java
+++ src/java/cool/GlobalError.java
@@ -4,12 +4,12 @@ package cool;
 
 public class GlobalError{
 
-	private boolean errorFlag = false;
+	private static boolean errorFlag = false;
 	public static void reportError(String filename, int lineNo, String error){
 		errorFlag = true;
 		System.err.println(filename+":"+lineNo+": "+error);
 	}
-	
+
 	public static boolean getErrorFlag(){
 		return errorFlag;
 	}
