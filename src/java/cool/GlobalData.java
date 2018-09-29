f126ff6d02f82813e8f3fb9fc5d50f081c03f859
Supreet Singh <supreet51.cs@gmail.com>
2018-09-29 17:39:53 +0530

    Added class for Global Data

---
+++ src/java/cool/GlobalData.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalData{
+    /**
+     * Map of Scope Tables for each class.
+     * Modify this data structure later.
+     */
+    public static Map<String, cool.ScopeTable<String>> scpTables = new HashMap<>();
+
+}
